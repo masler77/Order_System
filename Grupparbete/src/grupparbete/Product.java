3699c29238b2ed1fea47b4dd397d1898011639ab
Niclas <Niclas@Dauta>
2015-10-26 11:34:23 +0100

    Test

---
+++ Grupparbete/src/grupparbete/Product.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupparbete;
+
+/**
+ *
+ * @author Niclas
+ */
+public class Product {
+
+    int productID = 241513;
+    String productName = "Cykel";
+    double productPrice = 1200.00;
+
+    public int getProductID() { return productID;}
+    public String getProductName() { return productName; }
+    public double getProducPrice() { return productPrice; }
+    
+    
+public void showProduct() {
+
+
+    
+}
+
+}
