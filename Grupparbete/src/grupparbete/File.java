e6aa3e33e0c95fe813ce5d7820b9d5e9ff52e1fa
Niclas <Niclas@Dauta>
2015-10-26 11:34:12 +0100

    Test

---
+++ Grupparbete/src/grupparbete/File.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupparbete;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Niclas
+ */
+public class File {
+    
+
+
+    public void saveFile() {
+        
+
+    }
+
+    public void readFile() {
+    }
+
+}
